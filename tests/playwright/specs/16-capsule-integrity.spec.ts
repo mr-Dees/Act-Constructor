81bf78077b0aa5e4eceb3bdc213e7a01fde0cda4
mr-dees <dees-mr@yandex.ru>
2026-06-30 02:58:37 +0700

    Срезаны лишние BOM-байты в начале spec-файла капсул

--- tests/playwright/specs/16-capsule-integrity.spec.ts
+++ tests/playwright/specs/16-capsule-integrity.spec.ts
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import { openAct, SEED_ACTS } from '../fixtures';
 
 // Прогоняет validateAndRepairCapsules в браузере на заданном HTML.
